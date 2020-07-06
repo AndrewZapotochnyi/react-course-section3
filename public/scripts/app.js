02ff576d64dabf50df9306492b2a4069eacf335d
Andrew Zapotochnyi <andrewzapotochnyi@icloud.com>
2020-07-06 18:11:46 -0400

    todolist

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,117 +1,67 @@
-"use strict";
+'use strict';
 
-console.log("App.js is running");
+console.log('App.js is running!');
 
-// create app object title/subtitle
-// user title/subtitle in the template
-// render template
-
-// const title = {
-//   title: 'Indecision App',
-//   subtitle: 'Subtitle'
-// }
-
-// const template = (
-//   <div> 
-//     <h1 id="someid">{title.title}</h1> 
-//     <p>{title.subtitle}</p>
-//   </div>
-// );
-
-// Create a template a new JSX expression
-// root div
-//    h1 = Name
-//    p = Age: 35
-//    p = Location: Toronto
-// Render template 2
-
-// const user = {
-//   name: 'Andrew',
-//   age: 35,
-//   location: 'Toronto'
-// }
-
-// const userName = 'Andrew Zapotochnyi';
-// const userAge = 35;
-// const userLocation = "Toronto"
-
-// const getFirstName = (name) => name.split(' ')[0];
-
-// if (userName) {
-//   getFirstName(userName);
-// }
-
-// const template2 = (
-//   <div>
-//     <h1>{getFirstName(userName)}</h1>
-//     <p>Age: {user.age}</p>
-//     <p>Location: {user.location}</p>
-
-//   </div>
-// );
-
-var count = 0;
-var addOne = function addOne() {
-  // console.log("addOne");
-  count++;
-  renderCounterApp();
+var app = {
+  title: 'Indecision App',
+  subtitle: 'Put your life in the hands of a computer',
+  options: ['One', 'Two']
 };
 
-var minusOne = function minusOne() {
-  // console.log("minusOne");
-  count--;
-  renderCounterApp();
+var onFormSubmit = function onFormSubmit(event) {
+  event.preventDefault();
+
+  var option = event.target.elements.option.value;
+
+  if (option) {
+    app.options.push(option);
+  }
 };
 
-var reset = function reset() {
-  // console.log("reset");
-  count = 0;
-  renderCounterApp();
-};
+var template = React.createElement(
+  'div',
+  null,
+  React.createElement(
+    'h1',
+    null,
+    app.title
+  ),
+  app.subtitle && React.createElement(
+    'p',
+    null,
+    app.subtitle
+  ),
+  React.createElement(
+    'p',
+    null,
+    app.options.length > 0 ? 'Here are your options' : 'No options'
+  ),
+  React.createElement(
+    'ol',
+    null,
+    React.createElement(
+      'li',
+      null,
+      'Item one'
+    ),
+    React.createElement(
+      'li',
+      null,
+      'Item two'
+    )
+  ),
+  React.createElement(
+    'form',
+    { onSubmit: onFormSubmit },
+    React.createElement('input', { type: 'text', name: 'option' }),
+    React.createElement(
+      'button',
+      null,
+      'Add Option'
+    )
+  )
+);
 
 var appRoot = document.getElementById('app');
 
-var renderCounterApp = function renderCounterApp() {
-  var templateTwo = React.createElement(
-    "div",
-    null,
-    React.createElement(
-      "h1",
-      null,
-      "Count ",
-      count
-    ),
-    React.createElement(
-      "button",
-      { onClick: addOne },
-      "+1"
-    ),
-    React.createElement(
-      "button",
-      { onClick: minusOne },
-      "-1"
-    ),
-    React.createElement(
-      "button",
-      { onClick: reset },
-      "reset"
-    )
-  );
-
-  ReactDOM.render(templateTwo, appRoot);
-};
-
-renderCounterApp();
-
-// const multiplier = {
-//   // array of numbers
-//   numbers: [1, 2, 3, 4, 5],
-//   // multiplyBy
-//   multiplyBy: 100,
-//   // multiply 
-//   multiply() {
-//     return this.numbers.map((num) => this.multiplyBy * num);
-//   }
-// }
-
-// console.log(multiplier.multiply());
+ReactDOM.render(template, appRoot);
--- src/app.js
+++ src/app.js
@@ -1,102 +1,40 @@
-console.log("App.js is running")
+console.log('App.js is running!');
 
-// create app object title/subtitle
-// user title/subtitle in the template
-// render template
+const app = {
+  title: 'Indecision App',
+  subtitle: 'Put your life in the hands of a computer',
+  options: ['One', 'Two']
+};
 
-// const title = {
-//   title: 'Indecision App',
-//   subtitle: 'Subtitle'
-// }
+const onFormSubmit = (event) => {
+  event.preventDefault();
+  
 
-// const template = (
-//   <div> 
-//     <h1 id="someid">{title.title}</h1> 
-//     <p>{title.subtitle}</p>
-//   </div>
-// );
+  const option = event.target.elements.option.value;
 
-// Create a template a new JSX expression
-// root div
-//    h1 = Name
-//    p = Age: 35
-//    p = Location: Toronto
-// Render template 2
+  if (option) {
+    app.options.push(option);
+  }
 
-// const user = {
-//   name: 'Andrew',
-//   age: 35,
-//   location: 'Toronto'
-// }
-
-// const userName = 'Andrew Zapotochnyi';
-// const userAge = 35;
-// const userLocation = "Toronto"
-
-// const getFirstName = (name) => name.split(' ')[0];
-
-// if (userName) {
-//   getFirstName(userName);
-// }
-
-// const template2 = (
-//   <div>
-//     <h1>{getFirstName(userName)}</h1>
-//     <p>Age: {user.age}</p>
-//     <p>Location: {user.location}</p>
-
-//   </div>
-// );
-
-let count = 0;
-const addOne = () => {
-  // console.log("addOne");
-  count++;
-  renderCounterApp();
 }
 
-const minusOne = () => {
-  // console.log("minusOne");
-  count--;
-  renderCounterApp();
-}
+const template = (
+  <div>
+    <h1>{app.title}</h1>
+    {app.subtitle && <p>{app.subtitle}</p>}
+    <p>{app.options.length > 0 ? 'Here are your options' : 'No options'}</p>
+    <ol>
+      <li>Item one</li>
+      <li>Item two</li>
+    </ol>
 
-const reset = () => {
-  // console.log("reset");
-  count = 0;
-  renderCounterApp();
-}
+    <form onSubmit={onFormSubmit}>
+      <input type="text" name="option" />
+      <button>Add Option</button>
+    </form>
+  </div>
+);
 
 const appRoot = document.getElementById('app');
 
-const renderCounterApp = () => {
-  const templateTwo = (
-    <div>
-      <h1>Count {count}</h1>
-      <button onClick={addOne}>+1</button>
-      <button onClick={minusOne}>-1</button>
-      <button onClick={reset}>reset</button>
-    </div>
-  );
-
-  ReactDOM.render(templateTwo, appRoot);
-}
-
-
-renderCounterApp();
-
-
-
-// const multiplier = {
-//   // array of numbers
-//   numbers: [1, 2, 3, 4, 5],
-//   // multiplyBy
-//   multiplyBy: 100,
-//   // multiply 
-//   multiply() {
-//     return this.numbers.map((num) => this.multiplyBy * num);
-//   }
-// }
-
-// console.log(multiplier.multiply());
-
+ReactDOM.render(template, appRoot);
