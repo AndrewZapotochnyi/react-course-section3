8dae645b4c314b94514a0bf687479dd978330140
Andrew Zapotochnyi <andrewzapotochnyi@icloud.com>
2020-07-15 22:44:41 -0400

    Add class properties

---
+++ src/components/IndecisionApp.js
@@ -0,0 +1,91 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import AddOption from './AddOption';
+import Option from './Option';
+import Header from './Header';
+import Action from './Action';
+import Options from './Options'
+
+
+// stateless functional component
+
+export default class IndecisionApp extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.handleDeleteOptions = this.handleDeleteOptions.bind(this);
+    this.handlePick = this.handlePick.bind(this);
+    this.handleAddOption = this.handleAddOption.bind(this);
+    this.handleDeleteOption = this.handleDeleteOption.bind(this);
+    this.state = {
+      options: props.options
+    };
+  }
+
+  componentDidMount() {
+    console.log("Component did mounts")
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    console.log("Component did update")
+  }
+
+  componentWillUnmount() {
+    console.log("Component will unmount")
+  }
+
+  handleDeleteOptions() {
+    this.setState(() => ({ options: [] }));
+  }
+  handleDeleteOption(optionToRemove) {
+    this.setState((prevState) => ({
+      options: prevState.options.filter((option) => optionToRemove !== option)
+    }));
+  }
+  handlePick() {
+    const randomNum = Math.floor(Math.random() * this.state.options.length);
+    const option = this.state.options[randomNum];
+    alert(option);
+  }
+  handleAddOption(option) {
+    if (!option) {
+      return 'Enter valid value to add item';
+    } else if (this.state.options.indexOf(option) > -1) {
+      return 'This option already exists';
+    }
+
+    this.setState((prevState) => ({
+      options: prevState.options.concat(option)
+    }));
+  }
+  render() {
+    const subtitle = 'Put your life in the hands of a computer';
+
+    return (
+      <div>
+        <Header subtitle={subtitle} />
+        <Action
+          hasOptions={this.state.options.length > 0}
+          handlePick={this.handlePick}
+        />
+        <Options
+          options={this.state.options}
+          handleDeleteOptions={this.handleDeleteOptions}
+          handleDeleteOption={this.handleDeleteOption}
+        />
+        <AddOption
+          handleAddOption={this.handleAddOption}
+        />
+      </div>
+    );
+  }
+}
+
+IndecisionApp.defaultProps = {
+  options: []
+};
+
+Header.defaultProps = {
+  title: 'Indecision'
+};
