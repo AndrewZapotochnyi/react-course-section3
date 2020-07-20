756de1ae4f26e1c815d178bd52054101df5c22ac
Andrew Zapotochnyi <andrewzapotochnyi@icloud.com>
2020-07-20 14:34:16 -0400

    Redux 101 done

--- src/playground/redux-101.js
+++ src/playground/redux-101.js
@@ -1,15 +1,25 @@
 import { createStore } from 'redux';
 
-const incrementCount = (payload = {}) => ({
-  type: 'INCREMENT'
+const incrementCount = ({incrementBy} = 1) => ({
+  type: 'INCREMENT',
+  incrementBy: incrementBy
 });
+
+const decrementCount = ({decrementBy} = 1) => ({
+  type: 'DECREMENT',
+  decrementBy: decrementBy
+})
+
+const setCount = ({count}) => ({
+  type: 'SET',
+  count: count
+})
 
 const store = createStore((state = { count: 0 }, action) => {
   switch (action.type) {
     case 'INCREMENT': 
-      const incrementBy = typeof action.incrementBy === 'number' ? action.incrementBy : 1;
       return {
-        count: state.count + incrementBy
+        count: state.count + action.incrementBy
       };
     case 'DECREMENT':
       const decrementBy = typeof action.decrementBy === 'number' ? action.decrementBy : 1;
@@ -34,26 +44,32 @@ store.subscribe(() => {
   console.log(store.getState());
 })
 
+store.dispatch(incrementCount({incrementBy: 5}));
 
-store.dispatch({
-  type: 'INCREMENT',
-  incrementBy: 5
-});
+store.dispatch(decrementCount({decrementBy: 10}))
 
-store.dispatch({
-  type: 'DECREMENT',
-  decrementBy: 10
-});
+store.dispatch(setCount({ count: 101 }))
 
-store.dispatch({
-  type: 'DECREMENT'
-});
 
-store.dispatch({
-  type: 'RESET'
-});
+// store.dispatch({
+//   type: 'INCREMENT',
+//   incrementBy: 5
+// });
 
-store.dispatch({
-  type: 'SET',
-  count: 101
-})
+// store.dispatch({
+//   type: 'DECREMENT',
+//   decrementBy: 10
+// });
+
+// store.dispatch({
+//   type: 'DECREMENT'
+// });
+
+// store.dispatch({
+//   type: 'RESET'
+// });
+
+// store.dispatch({
+//   type: 'SET',
+//   count: 101
+// })
