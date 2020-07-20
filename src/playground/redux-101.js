9e3f1a230f36763490a6b0ca81e02890c92f48a7
Andrew Zapotochnyi <andrewzapotochnyi@icloud.com>
2020-07-20 14:09:05 -0400

    Redux refactoring

--- src/playground/redux-101.js
+++ src/playground/redux-101.js
@@ -1,18 +1,28 @@
 import { createStore } from 'redux';
 
+const incrementCount = (payload = {}) => ({
+  type: 'INCREMENT'
+});
+
 const store = createStore((state = { count: 0 }, action) => {
   switch (action.type) {
     case 'INCREMENT': 
+      const incrementBy = typeof action.incrementBy === 'number' ? action.incrementBy : 1;
       return {
-        count: state.count + 1
+        count: state.count + incrementBy
       };
     case 'DECREMENT':
+      const decrementBy = typeof action.decrementBy === 'number' ? action.decrementBy : 1;
       return {
-        count: state.count - 1
+        count: state.count - decrementBy
       }
     case 'RESET':
       return {
         count: 0
+      }
+    case 'SET':
+      return {
+        count: action.count
       }
     default:
       return state;
@@ -20,24 +30,30 @@ const store = createStore((state = { count: 0 }, action) => {
 
 });
 
-console.log(store.getState());
+store.subscribe(() => {
+  console.log(store.getState());
+})
+
 
 store.dispatch({
-  type: 'INCREMENT'
+  type: 'INCREMENT',
+  incrementBy: 5
+});
+
+store.dispatch({
+  type: 'DECREMENT',
+  decrementBy: 10
 });
 
 store.dispatch({
   type: 'DECREMENT'
 });
-
-store.dispatch({
-  type: 'DECREMENT'
-});
-
-console.log(store.getState());
 
 store.dispatch({
   type: 'RESET'
 });
 
-console.log(store.getState());
+store.dispatch({
+  type: 'SET',
+  count: 101
+})
