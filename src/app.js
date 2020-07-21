c161b613d0d7636ea77aeaa337e7828339ee2897
Andrew Zapotochnyi <andrewzapotochnyi@icloud.com>
2020-07-21 14:44:39 -0400

    Higher Order Component

--- src/app.js
+++ src/app.js
@@ -2,7 +2,26 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
+import configureStore from './store/configureStore';
+import { addExpense } from './actions/expenses';
+import { setTextFilter } from './actions/filters';
+import getVisibleExpenses from './selectors/expenses';
+
 
 import IndecisionApp from './components/IndecisionApp'
 
+const store = configureStore();
+
+
+
+store.dispatch(addExpense({ description: 'Watter bill'}));
+store.dispatch(addExpense({ description: 'Gas bill'}));
+store.dispatch(setTextFilter('bill'));
+
+console.log(store.getState());
+
+const state = store.getState();
+const visibleExpenses = getVisibleExpenses(state.expenses, state.filters);
+console.log(visibleExpenses);
+ 
 ReactDOM.render(<IndecisionApp />, document.getElementById('app'));
